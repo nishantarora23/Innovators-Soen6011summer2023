40cd84ba8b2801b6eb53cb187eaa2b6eb011eb99
Gagan Pandher <gaganpandher04@gmail.com>
2023-07-13 20:52:22 -0400

    #15 Added signup page for Student and AppRoute

--- web/src/views/AppRoutes.tsx
+++ web/src/views/AppRoutes.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage/LandingPage';
+import Login from './Login/Login';
+import SignUp from './SignUp/SignUp';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
