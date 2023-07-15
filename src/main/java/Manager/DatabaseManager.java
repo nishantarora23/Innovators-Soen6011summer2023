a6810cbaa0758fe681a704e46280269e81d94e98
Navdeep Kaur <kaurnav4199@gmail.com>
2023-07-15 15:50:50 -0400

    minor changes for Database manager

--- src/main/java/Manager/DatabaseManager.java
+++ src/main/java/Manager/DatabaseManager.java
@@ -3,31 +3,30 @@ package Manager;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class DatabaseManager {
-    private DataSource dataSource;
-    public DatabaseManager(String jndiname){
-        try {
-            Context context = (Context) new InitialContext().lookup("java:comp/env/" );
-            dataSource = (DataSource) context.lookup(jndiname);
+	private DataSource dataSource;
 
-        } catch (NamingException e) {
-            // Handle error that it's not configured in JNDI.
-            throw new IllegalStateException(jndiname + " is missing in JNDI!", e);
-        }
-    }
-    public Connection getConnection() {
-        try {
-            return dataSource.getConnection();
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+	public DatabaseManager(String jndiname) {
+		try {
+			dataSource = (DataSource) new InitialContext().lookup("java:comp/env/" + jndiname);
+		} catch (NamingException e) {
+			// Handle error that it's not configured in JNDI.
+			throw new IllegalStateException(jndiname + " is missing in JNDI!", e);
+		}
+	}
 
-        return null;
-    }
+	public Connection getConnection() {
+		try {
+			return dataSource.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
 }
