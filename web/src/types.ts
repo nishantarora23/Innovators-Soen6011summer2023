98a18876bc63c9e9e777ec35360f98dddf42b147
Gagan Pandher <gaganpandher04@gmail.com>
2023-07-15 22:23:12 -0400

    added College/University field to the signup form

--- web/src/types.ts
+++ web/src/types.ts
@@ -6,6 +6,7 @@ export type User = {
   dob: string;
   cName: string;
   username: string;
+  collegeName: string;
 };
 
 export enum RoleType {
--- web/src/views/SignUp/SignUp.tsx
+++ web/src/views/SignUp/SignUp.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useMemo, useState } from 'react';
-import { injectIntl } from 'react-intl';
-import { useNavigate, Link } from 'react-router-dom';
-import { Button, Grid } from '@mui/material';
-import RoleForm from '../../components/SignUp/RoleForm';
-import UserForm from '../../components/SignUp/UserForm';
-import DisclaimerForm from '../../components/SignUp/DisclaimerForm';
-import { RoleType } from '../../types';
-import { createUser } from '../../services/userService';
-import './SignUp.scss';
-import LoadingSpinner from '../../components/common/LoadingSpinner/LoadingSpinner';
-import MenuBar from '../../components/MenuBar/MenuBar';
-import FooterComp from '../../components/FooterComp/FooterComp';
-import AppSnackbar from '../../components/AppSnackbar/AppSnackbar';
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+import { useEffect, useMemo, useState } from "react";
+import { injectIntl } from "react-intl";
+import { useNavigate, Link } from "react-router-dom";
+import { Button, Grid } from "@mui/material";
+import RoleForm from "../../components/SignUp/RoleForm";
+import UserForm from "../../components/SignUp/UserForm";
+import DisclaimerForm from "../../components/SignUp/DisclaimerForm";
+import { RoleType } from "../../types";
+import { createUser } from "../../services/userService";
+import "./SignUp.scss";
+import LoadingSpinner from "../../components/common/LoadingSpinner/LoadingSpinner";
+import MenuBar from "../../components/MenuBar/MenuBar";
+import FooterComp from "../../components/FooterComp/FooterComp";
+import AppSnackbar from "../../components/AppSnackbar/AppSnackbar";
+import ArrowLeftIcon from "@mui/icons-material/ArrowLeft";
 type Props = {
   intl: any;
 };
@@ -26,20 +26,36 @@ const SignUp = ({ intl }: Props) => {
 
   //state for form data
   const [formData, setFormData] = useState({
-    fullName: '',
-    email: '',
-    password: '',
+    fullName: "",
+    email: "",
+    password: "",
     userRole: RoleType.STUDENT,
-    dob: new Date().toISOString().split('T')[0],
-    cName: '',
-    username: ''
+    dob: new Date().toISOString().split("T")[0],
+    cName: "",
+    username: "",
+    collegeName: "",
   });
 
   const signUpPages = useMemo(() => {
     return [
-      <RoleForm page={page} setPage={setPage} formData={formData} setFormData={setFormData} />,
-      <UserForm page={page} setPage={setPage} formData={formData} setFormData={setFormData} />,
-      <DisclaimerForm page={page} setPage={setPage} formData={formData} setFormData={setFormData} />
+      <RoleForm
+        page={page}
+        setPage={setPage}
+        formData={formData}
+        setFormData={setFormData}
+      />,
+      <UserForm
+        page={page}
+        setPage={setPage}
+        formData={formData}
+        setFormData={setFormData}
+      />,
+      <DisclaimerForm
+        page={page}
+        setPage={setPage}
+        formData={formData}
+        setFormData={setFormData}
+      />,
     ];
   }, [formData, page]);
 
@@ -49,7 +65,7 @@ const SignUp = ({ intl }: Props) => {
       await setLoading(true);
       await createUser(formData)
         .then((res) => {
-          navigate('/', { state: { ...res.data } });
+          navigate("/", { state: { ...res.data } });
         })
         .catch((err) => {
           setPage(page - 1);
@@ -70,18 +86,21 @@ const SignUp = ({ intl }: Props) => {
       <AppSnackbar
         type="error"
         message={intl.formatMessage({
-          id: 'userForm.createUser.error'
+          id: "userForm.createUser.error",
         })}
         open={isError}
       />
-      <MenuBar title={intl.formatMessage({ id: 'global.app_title' })} noBtn={true} />
+      <MenuBar
+        title={intl.formatMessage({ id: "global.app_title" })}
+        noBtn={true}
+      />
       <Grid container className="sign-up-container">
         <LoadingSpinner isOpen={loading} />
         <Grid container justifyContent="left">
-          <Button component={Link} to={'/'}>
+          <Button component={Link} to={"/"}>
             <ArrowLeftIcon />
             {intl.formatMessage({
-              id: 'global.back_to_home'
+              id: "global.back_to_home",
             })}
           </Button>
         </Grid>
