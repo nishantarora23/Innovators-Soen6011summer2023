93c0e99497ae0c95c32ff21dc368a80b7a3da00e
Navdeep Kaur <kaurnav4199@gmail.com>
2023-07-18 16:12:13 -0400

    #38 changes for add job offer

--- src/main/java/Servlets/JobServlet.java
+++ src/main/java/Servlets/JobServlet.java
@@ -4,14 +4,17 @@ import Commons.Helper;
 import Database.JobDAO;
 import Database.UserDAO;
 import Models.Job;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @WebServlet("/jobOffer")
 public class JobServlet {
@@ -33,5 +36,17 @@ public class JobServlet {
             throw e;
         }
         response.setStatus(HttpServletResponse.SC_OK);
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+        ArrayList<Job> jobs = JobDAO.getAllJobs();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(jobs);
+
+        // Set the content type of the response
+        response.setContentType("application/json");
+
+        // Write the JSON to the response
+        response.getWriter().write(json);
     }
 }
