940d5b61d26b985204d7eb33be408d99825f460c
Navdeep Kaur <kaurnav4199@gmail.com>
2023-07-22 20:23:38 -0400

    #35 changes for job offer modifications

--- src/main/java/Servlets/JobServlet.java
+++ src/main/java/Servlets/JobServlet.java
@@ -3,7 +3,6 @@ package Servlets;
 import Commons.Helper;
 import Database.JobDAO;
 import Models.Job;
-import Models.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -27,18 +26,86 @@ public class JobServlet extends HttpServlet {
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.setDateFormat("yyyy-MM-dd"); // Set your desired date format pattern
         Gson gson = gsonBuilder.create();
-        Job job = gson.fromJson(payloadData, Job.class);
-        System.out.println("Payload : " + payloadData);
-        try
-        {
-            JobDAO.addJob(job);
+
+        String action = request.getParameter("ACTION");
+        System.out.println("Action :" + action);
+        if("REMOVE".equals(action)){
+            JsonObject jsonPayload = new Gson().fromJson(payloadData, JsonObject.class);
+            try
+            {
+                JobDAO.remove(jsonPayload.get("id").getAsString());
+            }
+            catch (Exception e)
+            {
+                response.setStatus(HttpServletResponse.SC_EXPECTATION_FAILED);
+                throw new RuntimeException(e);
+            }
+            response.setStatus(HttpServletResponse.SC_OK);
         }
-        catch (Exception e)
-        {
-            response.setStatus(HttpServletResponse.SC_EXPECTATION_FAILED);
-            throw new RuntimeException(e);
+        else if("Update".equals(action)){
+            JsonObject jsonPayload = new Gson().fromJson(payloadData, JsonObject.class);
+            Job job = JobDAO.getJobId(jsonPayload.get("id").getAsString());
+            if(jsonPayload.get("contractType").getAsString() != null)
+            {
+                job.setContractType(jsonPayload.get("contractType").getAsString());
+            }
+            if(jsonPayload.get("deadline").getAsString() != null)
+            {
+                job.setDeadline(jsonPayload.get("deadline").getAsString());
+            }
+            if(jsonPayload.get("description").getAsString() != null)
+            {
+                job.setDescription(jsonPayload.get("description").getAsString());
+            }
+            if(jsonPayload.get("location").getAsString() != null)
+            {
+                job.setLocation(jsonPayload.get("location").getAsString());
+            }
+            if(jsonPayload.get("qualifications").getAsString() != null)
+            {
+                job.setQualifications(jsonPayload.get("qualifications").getAsString());
+            }
+            if(jsonPayload.get("responsibilities").getAsString() != null)
+            {
+                job.setResponsibilities(jsonPayload.get("responsibilities").getAsString());
+            }
+            if(jsonPayload.get("username").getAsString() != null)
+            {
+                job.setUsername(jsonPayload.get("username").getAsString());
+            }
+            if(jsonPayload.get("title").getAsString() != null)
+            {
+                job.setTitle(jsonPayload.get("title").getAsString());
+            }
+            if(jsonPayload.get("salaryRange").getAsString() != null)
+            {
+                job.setSalaryRange(jsonPayload.get("salaryRange").getAsString());
+            }
+            try
+            {
+                JobDAO.update(job);
+            }
+            catch (Exception e)
+            {
+                response.setStatus(HttpServletResponse.SC_EXPECTATION_FAILED);
+                throw new RuntimeException(e);
+            }
+            response.setStatus(HttpServletResponse.SC_OK);
         }
-        response.setStatus(HttpServletResponse.SC_OK);
+        else {
+            Job job = gson.fromJson(payloadData, Job.class);
+            try
+            {
+                JobDAO.addJob(job);
+            }
+            catch (Exception e)
+            {
+                response.setStatus(HttpServletResponse.SC_EXPECTATION_FAILED);
+                throw new RuntimeException(e);
+            }
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+
     }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
